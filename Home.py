3b45533282737756e7b8a52c90d047dc56b35b2a
Bilen <bilengebremariam189@gmail.com>
2026-03-28 15:37:18 +0300

    Few arrangements more

--- Home.py
+++ Home.py
@@ -9,7 +9,9 @@ def add_todo():
     todo_added = st.session_state["new_todo"].strip()
 
     if todo_added:
-        if todo_added + "\n" in todos:
+        clean_todos = [t.strip() for t in todos]
+
+        if todo_added in clean_todos:
             st.warning("⚠️ This todo already exists!")
         else:
             todos.append(todo_added + "\n")
@@ -18,16 +20,20 @@ def add_todo():
 
 st.title("My To-Do App")
 st.subheader("")
-st.write("")
-
-for index, todo in enumerate(todos):
-    if st.checkbox(todo, key=todo):
-        new_todos = todos.copy()
-        new_todos.pop(index)
-        functions.write_todos(new_todos)
-        del st.session_state[todo]
-        st.rerun()
+st.write("Use your To-Do app to control your tasks and be <b>more productive.</b>",
+         unsafe_allow_html=True)
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
+for index, todo in enumerate(todos):
+    key = f"{todo}_{index}"
+
+    if st.checkbox(todo.strip(), key=key):
+        new_todos = todos.copy()
+        new_todos.pop(index)
+        functions.write_todos(new_todos)
+        del st.session_state[key]
+        st.rerun()
+
+
 
